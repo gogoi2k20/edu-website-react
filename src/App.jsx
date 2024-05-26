2c3331d0071ede294f008b030edfcc081064cfc1
gogoi2k20 <gogoi2k20@gmail.com>
2024-05-27 00:54:31 +0530

    Random

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div className='container'>
+        <Title subTitle='OUR PROGRAM' title='What We Offer'/>
+        <Programs />
+        <About />
+      </div>
+    </>
+  )
+}
+
+export default App
